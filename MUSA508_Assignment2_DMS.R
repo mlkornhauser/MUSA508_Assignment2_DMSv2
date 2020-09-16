5a06acdc411ad2edced7ba8d2392f5b76d755453
mlkornhauser <70709544+mlkornhauser@users.noreply.github.com>
2020-09-16 13:28:44 -0400

    Update MUSA508_Assignment2_DMS.R

--- MUSA508_Assignment2_DMS.R
+++ MUSA508_Assignment2_DMS.R
@@ -3,7 +3,7 @@
 # Divya, Maddy & Sophia
 
 ###########
-# Set Up 
+# SET UP
 ###########
 
 # Load Libraries
@@ -12,6 +12,8 @@ library(tidyverse)
 library(tidycensus)
 library(sf)
 library(kableExtra)
+library(sp)
+library(rgdal) 
 
 options(scipen=999)
 options(tigris_class = "sf")
@@ -79,28 +81,85 @@ palette5 <- c("#f0f9e8","#bae4bc","#7bccc4","#43a2ca","#0868ac")
 
 census_api_key("53845fc057d94b7ce8d243f50bd9fa0d9237c612", overwrite = TRUE)
 
-##################
-# Year 2009 tracts 
-##################
+#########################
+# 2009 & 2017 CENSUS DATA
+#########################
 
+#Pulling 2009 Census data for Allegheny County
 tracts09 <-  
   get_acs(geography = "tract", variables = c("B25026_001E", "B02001_002E", "B15001_050E", "B15001_009E",
                                              "B19013_001E", "B25058_001E", "B06012_002E"), 
-          year=2009, state=42, county=003, geometry=T) %>% 
-  st_transform('ESRI:102728')  
+          year=2009, state=42, county=003, geometry=T, output = "wide") %>% 
+  st_transform('ESRI:102728')%>%
+  rename(TotalPop = B25026_001E, 
+         Whites = B02001_002E,
+         FemaleBachelors = B15001_050E, 
+         MaleBachelors = B15001_009E,
+         MedHHInc = B19013_001E, 
+         MedRent = B25058_001E,
+         TotalPoverty = B06012_002E) %>%
+  dplyr::select(-NAME, -starts_with("B")) %>%
+  mutate(pctWhite = ifelse(TotalPop > 0, Whites / TotalPop,0),
+         pctBachelors = ifelse(TotalPop > 0, ((FemaleBachelors + MaleBachelors) / TotalPop),0),
+         pctPoverty = ifelse(TotalPop > 0, TotalPoverty / TotalPop, 0),
+         year = "2017") %>%
+  dplyr::select(-Whites, -FemaleBachelors, -MaleBachelors, -TotalPoverty)  
 
 #MK Note:  Need to select just the tracts within Pittsburgh City limits
 
-head(tracts09)
+summary(tracts09)
 
-##################
-# Year 2017 tracts 
-##################
-
+#Pulling 2017 Census data for Allegheny County
 tracts17 <-  
   get_acs(geography = "tract", variables = c("B25026_001E", "B02001_002E", "B15001_050E", "B15001_009E",
                                              "B19013_001E", "B25058_001E", "B06012_002E"), 
-          year=2017, state=42, county=003, geometry=T) %>% 
-  st_transform('ESRI:102728')  
+          year=2017, state=42, county=003, geometry=T, output = "wide") %>% 
+  st_transform('ESRI:102728') %>%
+  rename(TotalPop = B25026_001E, 
+         Whites = B02001_002E,
+         FemaleBachelors = B15001_050E, 
+         MaleBachelors = B15001_009E,
+         MedHHInc = B19013_001E, 
+         MedRent = B25058_001E,
+         TotalPoverty = B06012_002E) %>%
+  dplyr::select(-NAME, -starts_with("B")) %>%
+  mutate(pctWhite = ifelse(TotalPop > 0, Whites / TotalPop,0),
+         pctBachelors = ifelse(TotalPop > 0, ((FemaleBachelors + MaleBachelors) / TotalPop),0),
+         pctPoverty = ifelse(TotalPop > 0, TotalPoverty / TotalPop, 0),
+         year = "2017") %>%
+  dplyr::select(-Whites, -FemaleBachelors, -MaleBachelors, -TotalPoverty)
+
+#MK Note:  Need to select just the tracts within Pittsburgh City limits
 
 head(tracts17)
+summary(tracts17)
+
+# Combine 2009 and 2017 census data into single dataframe
+
+allTracts <- rbind(tracts09,tracts17)
+summary(allTracts)
+
+###############
+# TRANSIT DATA
+###############
+#Load in lightrail data and rename column headers
+LightRailPGH <- read.csv('./data/LightRailPGH.csv')%>%
+  select(Stop_name.C.254, Direction.C.254, Routes_ser.C.254, Latitude.N.19.11, Longitude.N.19.11) %>%
+  rename(Stopname=Stop_name.C.254, 
+         Direction=Direction.C.254, 
+         Routes=Routes_ser.C.254, 
+         Lat=Latitude.N.19.11, 
+         Lon=Longitude.N.19.11) 
+
+#Convert lat and longitude to point geometry
+#Stuck on this
+#latlong_sf <- st_as_sf(LightRailPGH, coords = ___, crs = ___)
+
+
+xy <- as.data.frame(cbind(X = LightRailPGH$Lat, Y = LightRailPGH$Lon))
+coordinates(xy) <- c("X", "Y")
+proj4string(xy) <- CRS("+proj=longlat +datum=WGS84") 
+res <- spTransform(xy, CRS("+proj=utm +zone=51 ellps=WGS84"))
+head(res)
+
+
